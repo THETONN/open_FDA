fba4f81e0776a81e6fa62ee96344b07e60c0cc66
THETONN <tonkingrat@gmail.com>
2025-06-20 10:41:36 +0700

    update openAI clean drug

--- ntbks/openFDA_drug_event_parsing/openAI_clean_drug.py
+++ ntbks/openFDA_drug_event_parsing/openAI_clean_drug.py
@@ -19,16 +19,17 @@ client = OpenAI(api_key=api_key)
 
 def get_clean_drug_name(drug_name):
     prompt = f"""
-แยกชื่อยาหลักจากข้อความต่อไปนี้ เพื่อใช้ค้นหาในฐานข้อมูลยา เช่น RxNorm:
-ชื่อยา: {drug_name}
-โปรดตอบกลับเฉพาะชื่อยาหลักเท่านั้น เช่น: Acetaminophen
-"""
+    Extract the primary (generic) drug name from the following text. 
+    Respond ONLY with the drug name and nothing else.
+
+    Raw drug text: {drug_name}
+    """
     try:
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
+            model="gpt-4o",
             messages=[{"role": "user", "content": prompt}],
-            temperature=0.2,
-            max_tokens=50
+            temperature=0.1,
+            max_tokens=30
         )
         return response.choices[0].message.content.strip()
     except Exception as e:
@@ -47,13 +48,13 @@ def extract_drug_name_from_gpt(raw_response: str) -> str:
     return raw_response.strip()
 
 
-batch_size = 1000
+batch_size = 3200
 pause_time = 120  
 total = len(drug_df)
 
 for start in range(0, total, batch_size):
     end = min(start + batch_size, total)
-    print(f"\n🌀 Processing batch {start} to {end - 1}...\n")
+    print(f"\n Processing batch {start} to {end - 1}...\n")
 
     for i in range(start, end):
         if pd.notna(drug_df.loc[i, 'medicinal_product_clean']):
@@ -69,12 +70,12 @@ for start in range(0, total, batch_size):
             
             drug_df.to_csv("../../data/intermediate_cleaned.csv", index=False)
 
-        time.sleep(1.0)  
+        time.sleep(1)  
 
-    # save full batch
+    
     drug_df.to_csv(f"../../data/batch_cleaned_{start}_{end}.csv", index=False)
 
-    print(f"✅ Batch {start}-{end-1} complete. Pausing {pause_time} sec...\n")
+    print(f"Batch {start}-{end-1} complete. Pausing {pause_time} sec...\n")
     time.sleep(pause_time)
 
 drug_df.to_csv("../../data/drug_cleaned_final.csv", index=False)
